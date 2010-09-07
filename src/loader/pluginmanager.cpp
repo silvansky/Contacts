21ad8e3cbf8f015d204de334e98a3e5dd6f491d6
s.potapov <s.potapov@d44c6877-a352-904b-aa75-5222c7c0a737>
2010-09-07 18:59:25 +0000

    Из файла описания плагинов не удалялись записи удаленных плагинов

--- src/loader/pluginmanager.cpp
+++ src/loader/pluginmanager.cpp
@@ -735,7 +735,7 @@ void PluginManager::removePluginsInfo(const QStringList &ACurFiles)
 		{
 			QDomElement oldElem = pluginElem;
 			pluginElem = pluginElem.nextSiblingElement();
-			pluginElem.parentNode().removeChild(oldElem);
+			oldElem.parentNode().removeChild(oldElem);
 		}
 		else
 			pluginElem = pluginElem.nextSiblingElement();
