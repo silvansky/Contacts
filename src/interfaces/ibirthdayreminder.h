1ccda994b9c9bdd316d305b41366b7f4f6647974
s.potapov <s.potapov@d44c6877-a352-904b-aa75-5222c7c0a737>
2010-11-18 15:47:43 +0000

    Обновлен интерфейс IBirthdayReminder.

--- src/interfaces/ibirthdayreminder.h
+++ src/interfaces/ibirthdayreminder.h
@@ -1,12 +1,19 @@
 #ifndef IBIRTHDAYREMINDER_H
 #define IBIRTHDAYREMINDER_H
 
+#include <QDate>
+#include <QImage>
+#include <utils/jid.h>
+
 #define BIRTHDAYREMINDER_UUID "78E8CA6C-99F0-4F38-B52B-3FF522B7F110}"
 
 class IBirthdayReminder
 {
 public:
 	virtual QObject *instance() =0;
+	virtual QDate contactBithday(const Jid &AContactJid) const =0;
+	virtual int contactBithdayDaysLeft(const Jid &AContactJid) const =0;
+	virtual QImage avatarWithCake(const Jid &AContactJid, QImage &AAvatar = QImage()) const =0;
 };
 
 Q_DECLARE_INTERFACE(IBirthdayReminder,"Virtus.Plugin.IBirthdayReminer/1.0")
