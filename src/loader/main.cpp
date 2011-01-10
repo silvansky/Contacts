ff301515feeff77553c3ee2176123f185e1df562
vgorshkov <vgorshkov@d44c6877-a352-904b-aa75-5222c7c0a737>
2011-01-10 12:05:43 +0000

    Разобрался с глюком отображения задизейбленных комбо бокса и кнопки (убрана тень текста с помощью QProxyStyle).

--- src/loader/main.cpp
+++ src/loader/main.cpp
@@ -1,12 +1,28 @@
 #include <QLibrary>
 #include <QApplication>
+#include <QProxyStyle>
 #include "pluginmanager.h"
+
+// proxy style
+
+class ProxyStyle : public QProxyStyle
+{
+public:
+	int styleHint(StyleHint hint, const QStyleOption *option = 0,
+		      const QWidget *widget = 0, QStyleHintReturn *returnData = 0) const
+	{
+		if (hint == QStyle::SH_EtchDisabledText || hint == QStyle::SH_DitherDisabledText)
+			return 0;
+		return QProxyStyle::styleHint(hint, option, widget, returnData);
+	}
+};
 
 int main(int argc, char *argv[])
 {
 	QApplication app(argc, argv);
 	app.setQuitOnLastWindowClosed(false);
 	app.addLibraryPath(app.applicationDirPath());
+	app.setStyle(new ProxyStyle);
 	QLibrary utils(app.applicationDirPath()+"/utils",&app);
 	utils.load();
 	PluginManager pm(&app);
