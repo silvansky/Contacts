f8d24bc21d594b8ce0aeaa9cdba70c65b85c5ba6
s.popov <s.popov@d44c6877-a352-904b-aa75-5222c7c0a737>
2011-01-15 12:27:00 +0000

    Добавлены библиотеки SIP, VoIPMedia, VoIPVideo пока! в виде сторонних dll без исходников.

---
+++ src/thirdparty/sipexternal/VoIPVideoLib/Inc/RCameraFlow.h
@@ -0,0 +1,40 @@
+#ifndef RCAMERAFLOW_H
+#define RCAMERAFLOW_H
+
+#include <QObject>
+#include <QImage>
+#include <QTimerEvent>
+//#include <opencv/cv.h>
+//#include <opencv/highgui.h>
+
+class videoInput;
+
+class RCameraFlow : public QObject
+{
+  Q_OBJECT
+
+public:
+  RCameraFlow(QObject *parent);
+  ~RCameraFlow();
+
+public:
+  QImage currentFrame() const;
+  bool isOk() const;
+
+protected:
+  void timerEvent(QTimerEvent*);
+
+private:
+  //void toQImage(IplImage *cvimage);
+  void toQImage(uchar* pixels, int width, int height);
+
+private:
+  int _flowTimer;
+  bool _cameraStart;
+  //CvCapture *_camera;
+  QImage _frame;
+
+  videoInput* pVInput;
+};
+
+#endif // RCAMERAFLOW_H
