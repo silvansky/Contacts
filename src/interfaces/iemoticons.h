6fd0399c185b5800498d1a8d8fcb97086c005041
s.potapov <s.potapov@d44c6877-a352-904b-aa75-5222c7c0a737>
2011-02-21 17:33:16 +0000

    Доработан интерфейс IEmoticons, теперь можно найти текстовые и графические смайлы в произвольном документе.

--- src/interfaces/iemoticons.h
+++ src/interfaces/iemoticons.h
@@ -2,9 +2,11 @@
 #define IEMOTICONS_H
 
 #include <QUrl>
+#include <QMap>
 #include <QIcon>
 #include <QString>
 #include <QStringList>
+#include <QTextDocument>
 
 #define EMOTICONS_UUID "{567dda17-ae34-4392-b6f1-d21320af994b}"
 
@@ -15,6 +17,8 @@ public:
 	virtual QList<QString> activeIconsets() const =0;
 	virtual QUrl urlByKey(const QString &AKey) const =0;
 	virtual QString keyByUrl(const QUrl &AUrl) const =0;
+	virtual QMap<int, QString> findTextEmoticons(const QTextDocument *ADocument, int AStartPos=0, int ALength=-1) const =0;
+	virtual QMap<int, QString> findImageEmoticons(const QTextDocument *ADocument, int AStartPos=0, int ALength=-1) const =0;
 };
 
 Q_DECLARE_INTERFACE(IEmoticons,"Virtus.Plugin.IEmoticons/1.0")
