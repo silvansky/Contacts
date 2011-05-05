6da056b0ba4935e2abf2d50f18ae411c42e260db
s.potapov <s.potapov@d44c6877-a352-904b-aa75-5222c7c0a737>
2011-05-05 19:33:30 +0000

    Новый порядок сервисов во вкладке мета-контактов.

--- src/definitions/metaitemorders.h
+++ src/definitions/metaitemorders.h
@@ -3,17 +3,17 @@
 
 #define MIO_RAMBLER              100
 #define MIO_ICQ                  150
-#define MIO_MAGENT               200
-#define MIO_YONLINE              250
-#define MIO_GTALK                300
-#define MIO_QIP                  350
-#define MIO_VKONTAKTE            400
-#define MIO_ODNOKLASNIKI         450
-#define MIO_LIVEJOURNAL          500
-#define MIO_FACEBOOK             550
-#define MIO_TWITTER              600
-#define MIO_FRING                650
-#define MIO_JABBER               700
+#define MIO_YONLINE              200
+#define MIO_GTALK                250
+#define MIO_VKONTAKTE            300
+#define MIO_FACEBOOK             350
+#define MIO_ODNOKLASNIKI         400
+#define MIO_LIVEJOURNAL          450
+#define MIO_MAGENT               500
+#define MIO_QIP                  550
+#define MIO_JABBER               600
+#define MIO_TWITTER              650
+#define MIO_FRING                700
 #define MIO_MAIL                 750
 #define MIO_SMS                  800
 
