18abe380a717076473642998d8c2f1ef5927c5cf
vgorshkov <vgorshkov@d44c6877-a352-904b-aa75-5222c7c0a737>
2011-06-22 13:06:38 +0000

    Версия 0.2.5

--- src/definitions/version.h
+++ src/definitions/version.h
@@ -2,7 +2,7 @@
 #define DEF_VERSION_H
 
 #define CLIENT_NAME             "Contacts"
-#define CLIENT_VERSION          "0.2.4"
+#define CLIENT_VERSION          "0.2.5"
 #define CLIENT_VERSION_SUFIX    ""
 #define CLIENT_HOME_PAGE        "http://contacts.rambler.ru"
 
