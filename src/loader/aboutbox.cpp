34dcba5781c81e444768de9579990ebf3d2d7ff7
vgorshkov <vgorshkov@d44c6877-a352-904b-aa75-5222c7c0a737>
2011-06-30 15:11:38 +0000

    Лого с бетой в окне входа и в окне о программе.

--- src/loader/aboutbox.cpp
+++ src/loader/aboutbox.cpp
@@ -4,18 +4,21 @@
 #include <QDesktopServices>
 #include <utils/customborderstorage.h>
 #include <utils/stylestorage.h>
+#include <utils/iconstorage.h>
 #include <utils/graphicseffectsstorage.h>
 #include <definitions/resources.h>
 #include <definitions/customborder.h>
 #include <definitions/stylesheets.h>
 #include <definitions/graphicseffects.h>
+#include <definitions/menuicons.h>
 
 AboutBox::AboutBox(IPluginManager *APluginManager, QWidget *AParent) : QDialog(AParent)
 {
 	ui.setupUi(this);
 	setAttribute(Qt::WA_DeleteOnClose,true);
 
-	ui.lblName->setText(tr("Contacts"));
+	//ui.lblName->setText(tr("Contacts"));
+	IconStorage::staticStorage(RSR_STORAGE_MENUICONS)->insertAutoIcon(ui.lblName, MNI_OPTIONS_LOGIN_LOGO, 0, 0, "pixmap");
 	ui.lblVersion->setText(tr("Version %1.%2 %3").arg(APluginManager->version()).arg(APluginManager->revision()).arg(CLIENT_VERSION_SUFIX).trimmed());
 	ui.lblHomePage->setText(tr("Official site: %1").arg("<a href='http://contacts.rambler.ru'>http://contacts.rambler.ru</a>"));
 	ui.lblCopyright->setText(tr("Copyright 2010-2011, \"Rambler Internet Holding Ltd\". All rights reserved.<br>%1").arg(QString("<a href='http://contacts.rambler.ru'>%1</a>").arg(tr("Terms of Use"))));
