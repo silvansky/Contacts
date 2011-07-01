30006259df13c4cd87cf35adb2e0d4809a94f332
vgorshkov <vgorshkov@d44c6877-a352-904b-aa75-5222c7c0a737>
2011-07-01 13:24:52 +0000

    Версия 0.2.10 beta.

--- src/definitions/version.h
+++ src/definitions/version.h
@@ -2,7 +2,7 @@
 #define DEF_VERSION_H
 
 #define CLIENT_NAME             "Contacts"
-#define CLIENT_VERSION          "0.2.9"
+#define CLIENT_VERSION          "0.2.10"
 #define CLIENT_VERSION_SUFIX    "beta"
 #define CLIENT_HOME_PAGE        "http://contacts.rambler.ru"
 
