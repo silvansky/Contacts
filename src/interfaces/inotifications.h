8bb3e4a0582e7cde64cfe0bf9f467c9fcc785820
s.potapov <s.potapov@d44c6877-a352-904b-aa75-5222c7c0a737>
2012-01-23 18:26:29 +0000

    Поправлены отступы.

--- src/interfaces/inotifications.h
+++ src/interfaces/inotifications.h
@@ -11,41 +11,41 @@
 
 struct INotification
 {
-    enum NotifyKinds {
-        RosterNotify          = 0x0001,
-        PopupWindow           = 0x0002,
-        TrayNotify            = 0x0004,
-        SoundPlay             = 0x0008,
-        AlertWidget           = 0x0010,
-        ShowMinimized         = 0x0020,
-        TabPageNotify         = 0x0040,
+	enum NotifyKinds {
+		RosterNotify          = 0x0001,
+		PopupWindow           = 0x0002,
+		TrayNotify            = 0x0004,
+		SoundPlay             = 0x0008,
+		AlertWidget           = 0x0010,
+		ShowMinimized         = 0x0020,
+		TabPageNotify         = 0x0040,
 #ifdef Q_WS_MAC
-        DockBadge             = 0x0080,
+		DockBadge             = 0x0080,
 #endif
-        AutoActivate          = 0x8000
-    };
-    enum NotifyFlags {
-        RemoveInvisible       = 0x0001,
-        TestNotify            = 0x8000
-    };
-    INotification() {
-        kinds = 0;
-        flags = RemoveInvisible;
-    }
-    QString typeId;
-    ushort kinds;
-    ushort flags;
-    QList<Action *> actions;
-    QMap<int, QVariant> data;
+		AutoActivate          = 0x8000
+	};
+	enum NotifyFlags {
+		RemoveInvisible       = 0x0001,
+		TestNotify            = 0x8000
+	};
+	INotification() {
+		kinds = 0;
+		flags = RemoveInvisible;
+	}
+	QString typeId;
+	ushort kinds;
+	ushort flags;
+	QList<Action *> actions;
+	QMap<int, QVariant> data;
 };
 
 struct INotificationType
 {
-    INotificationType() {
-        order = 0;
-        kindMask = 0;
-        kindDefs = 0;
-    }
+	INotificationType() {
+		order = 0;
+		kindMask = 0;
+		kindDefs = 0;
+	}
     int order;
     QString title;
     ushort kindMask;
@@ -55,30 +55,30 @@ struct INotificationType
 class INotifications
 {
 public:
-    virtual QObject *instance() =0;
-    virtual QList<int> notifications() const =0;
-    virtual INotification notificationById(int ANotifyId) const =0;
-    virtual int appendNotification(const INotification &ANotification) =0;
-    virtual void activateNotification(int ANotifyId) =0;
-    virtual void removeNotification(int ANotifyId) =0;
-    virtual void registerNotificationType(const QString &ATypeId, const INotificationType &AType) =0;
-    virtual QList<QString> notificationTypes() const =0;
-    virtual INotificationType notificationType(const QString &ATypeId) const =0;
-		virtual void removeNotificationType(const QString &ATypeId) =0;
-		virtual ushort enabledNotificationKinds() const =0;
-		virtual void setEnabledNotificationKinds(ushort AKinds) =0;
-    virtual ushort enabledTypeNotificationKinds(const QString &ATypeId) const =0;
-		virtual ushort typeNotificationKinds(const QString &ATypeId) const =0;
-    virtual void setTypeNotificationKinds(const QString &ATypeId, ushort AKinds) =0;
-    virtual QImage contactAvatar(const Jid &AStreamJid,const Jid &AContactJid) const =0;
-    virtual QIcon contactIcon(const Jid &AStreamJid, const Jid &AContactJid) const =0;
-    virtual QString contactName(const Jid &AStreamJId, const Jid &AContactJid) const =0;
+	virtual QObject *instance() =0;
+	virtual QList<int> notifications() const =0;
+	virtual INotification notificationById(int ANotifyId) const =0;
+	virtual int appendNotification(const INotification &ANotification) =0;
+	virtual void activateNotification(int ANotifyId) =0;
+	virtual void removeNotification(int ANotifyId) =0;
+	virtual void registerNotificationType(const QString &ATypeId, const INotificationType &AType) =0;
+	virtual QList<QString> notificationTypes() const =0;
+	virtual INotificationType notificationType(const QString &ATypeId) const =0;
+	virtual void removeNotificationType(const QString &ATypeId) =0;
+	virtual ushort enabledNotificationKinds() const =0;
+	virtual void setEnabledNotificationKinds(ushort AKinds) =0;
+	virtual ushort enabledTypeNotificationKinds(const QString &ATypeId) const =0;
+	virtual ushort typeNotificationKinds(const QString &ATypeId) const =0;
+	virtual void setTypeNotificationKinds(const QString &ATypeId, ushort AKinds) =0;
+	virtual QImage contactAvatar(const Jid &AStreamJid,const Jid &AContactJid) const =0;
+	virtual QIcon contactIcon(const Jid &AStreamJid, const Jid &AContactJid) const =0;
+	virtual QString contactName(const Jid &AStreamJId, const Jid &AContactJid) const =0;
 protected:
-    virtual void notificationActivated(int ANotifyId) =0;
-    virtual void notificationRemoved(int ANotifyId) =0;
-    virtual void notificationAppend(int ANotifyId, INotification &ANotification) =0;
-    virtual void notificationAppended(int ANotifyId, const INotification &ANotification) =0;
-    virtual void notificationTest(const QString &ATypeId, ushort AKinds) =0;
+	virtual void notificationActivated(int ANotifyId) =0;
+	virtual void notificationRemoved(int ANotifyId) =0;
+	virtual void notificationAppend(int ANotifyId, INotification &ANotification) =0;
+	virtual void notificationAppended(int ANotifyId, const INotification &ANotification) =0;
+	virtual void notificationTest(const QString &ATypeId, ushort AKinds) =0;
 };
 
 Q_DECLARE_INTERFACE(INotifications,"Virtus.Plugin.INotifications/1.0")
