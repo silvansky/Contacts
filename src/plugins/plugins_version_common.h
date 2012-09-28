e28bc4ff9c50cce323c81d7054e67b9f1e24032e
silvansky <v.silvansky@gmail.com>
2012-09-28 11:21:48 +0400

    0.7.0

--- src/plugins/plugins_version_common.h
+++ src/plugins/plugins_version_common.h
@@ -1,11 +1,11 @@
 #ifndef PLUGINS_VERSION_COMMON_H
 #define PLUGINS_VERSION_COMMON_H
 
-#define VER_FILEVERSION             0,6,10,0
-#define VER_FILEVERSION_STR         "0.6.10.0\0"
+#define VER_FILEVERSION             0,7,0,0
+#define VER_FILEVERSION_STR         "0.7.0.0\0"
 
-#define VER_PRODUCTVERSION          0,6,10,1868
-#define VER_PRODUCTVERSION_STR      "0.6.10\0"
+#define VER_PRODUCTVERSION          0,7,0,1875
+#define VER_PRODUCTVERSION_STR      "0.7.0\0"
 
 #define VER_COMPANYNAME_STR         "Rambler Internet Holding LLC"
 #define VER_LEGALCOPYRIGHT_STR      "Copyright © 2010-2012 Rambler Internet Holding LLC"
